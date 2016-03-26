4a009657f261706d31342668c69ca6db2c69378e
sky <cl98914@alibaba-inc.com>
2016-03-26 17:12:53 +0800

    init

---
+++ engine/tags/script.js
@@ -0,0 +1,22 @@
+var exports = module.exports;
+
+exports.compile = function(compiler, args, content, parents, options, blockName) {
+
+    var config = options.config||{};
+    //console.log('script parents %o', parents);
+    //console.log('script options %o', options);
+
+    //console.log('[script] fisRootDir:' + config.fisRootDir  + ' projectDir:'+config.projectDir + ' viewsDir:' + config.viewsDir);
+
+    var code = '_ctx.fiswig.addScript((function () { var _output = "";' +
+        compiler(content, parents, options, blockName) +
+        ' return _output; })());';
+    //console.log('>>>script:' + code);
+    return code;
+};
+
+exports.parse = function(str, line, parser, types) {
+    return true;
+};
+
+exports.ends = true;
